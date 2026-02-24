360467daf21c5e74526ce950c0491496401dce60
Mushfik Sami Siam <mushfiksami7701@gmail.com>
2026-02-24 19:59:30 +0600

    Fixed the embedding with local embedder

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ import os
 from langchain_text_splitters import RecursiveCharacterTextSplitter 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI 
 from langchain_community.vectorstores import FAISS 
-from langchain.prompts import PromptTemplate 
+from langchain_core.prompts import PromptTemplate 
 from langchain_classic.chains.question_answering import load_qa_chain
 import google.generativeai as genai 
-
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
 load_dotenv()
 
-genai.cofigure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model=HuggingFaceBgeEmbeddings(model_name='all-MiniLM-L6-v2')
 
 
 def get_pdf_text(pdf_docs):
@@ -26,12 +28,12 @@ def get_pdf_text(pdf_docs):
 
 def get_text_chunks(text):
     splitter=RecursiveCharacterTextSplitter(chunk_size=5000,chunk_overlap=1000)
-    chunks=splitter.split(text)
+    chunks=splitter.split_text(text)
     return chunks 
 
 
 def get_vector_store(text_chunks):
-    embedding=GoogleGenerativeAIEmbeddings(model='models/embedding-001')
+    embedding=model
     vector_store=FAISS.from_texts(text_chunks,embedding)
     vector_store.save_local('faiss_store')
 
@@ -52,9 +54,9 @@ def get_conversational_chain():
 
 
 def user_input(user_query):
-    embedding=GoogleGenerativeAIEmbeddings(model='models/embedding-001')
-    new_db=FAISS.load_local('faiss_store')
-    docs=new_db.similarity_search(user_query,embedding=embedding)
+    embedding=model
+    new_db=FAISS.load_local('faiss_store',embeddings=embedding,allow_dangerous_deserialization=True)
+    docs=new_db.similarity_search(user_query)
     chain=get_conversational_chain()
     response=chain(
         {'input_documents':docs,
@@ -71,7 +73,7 @@ def main():
     st.set_page_config('DOC CHATBOT')
     st.header('GEMINI DOCS CHATBOT')
 
-    user_question=st.input_text('Ask a question from the pdf') 
+    user_question=st.text_input('Ask a question from the pdf') 
 
     if user_question:
         user_input(user_question) 
@@ -79,7 +81,7 @@ def main():
     with st.sidebar:
         st.title("Menu") 
         pdf_docs=st.file_uploader('Upload multiple docs',accept_multiple_files=True)
-        with st.button('Submit and process'):
+        if st.button('Submit and process'):
             texts=get_pdf_text(pdf_docs)
             text_chunks=get_text_chunks(texts)
             get_vector_store(text_chunks)
