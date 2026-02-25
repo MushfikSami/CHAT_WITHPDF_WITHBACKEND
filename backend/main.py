7407695e55d5598244711f396fa0a4c16ef57948
Mushfik Sami Siam <janowar_10@Mushfiks-MacBook-Air.local>
2026-02-25 10:44:35 +0600

    Added a demo timer middleware

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
-from fastapi import FASTAPI 
+from fastapi import FASTAPI,Request 
 from api.routes import router 
+import time
+from prometheus_fastapi_instrumentator import Instrumentator
 
 
 app=FASTAPI()
+
+@app.middleware('http')
+async def add_process_time_header(request:Request,call_next):
+    start_time=time.time()
+    response= await call_next(request)
+    process_time=time.time()-start_time 
+
+    response.headers['X-Process-Time']=str(process_time)
+    return response  
+
+
+
+
+Instrumentator().instrument(app).expose(app)
+
 app.include_router(router)
